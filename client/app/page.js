5ca76c0b035f6bcdf179109ddf27145393876f0e
kale <18kmkf@queensu.ca>
2025-02-18 12:35:07 -0500

    feat: Simple client server connection

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState("Loading");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
+
   return (
     <div>
       <h1>On My Shelf</h1>
+      <p>{message}</p>
     </div>
   );
 }
