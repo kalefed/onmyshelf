86632d688bcb4a6373700e2e284bb79f98106d1e
kale <18kmkf@queensu.ca>
2025-05-12 13:35:05 -0400

    Add basic styling for an add book form

--- client/components/AddBookForm/AddBookForm.js
+++ client/components/AddBookForm/AddBookForm.js
@@ -2,21 +2,31 @@ import styles from "./AddBookForm.module.css";
 
 export default function AddBookForm() {
   return (
-    <form className={styles.form}>
+    <form className={styles["book-form"]}>
       <h1>Add a new book</h1>
-      <div className="flex flex-col">
-        <label htmlFor="title">Ttitle</label>
-        <input type="text" id="title" name="title" />
-        <p>Jade City</p>
-      </div>
-      <div>
-        <label htmlFor="author">Author</label>
+      <div className={styles["book-form__group"]}>
+        <label className={styles["book-form__label"]} htmlFor="title">
+          Title
+        </label>
         <input
+          className={styles["book-form__input"]}
+          type="text"
+          id="title"
+          name="title"
+        />
+        <p className={styles["book-form__placeholder"]}>Jade City</p>
+      </div>
+      <div className={styles["book-form__group"]}>
+        <label className={styles["book-form__label"]} htmlFor="author">
+          Author
+        </label>
+        <input
+          className={styles["book-form__input"]}
           type="text"
           id="author"
           name="author"
-          placeholder="Fonda Lee"
         ></input>
+        <p className={styles["book-form__placeholder"]}>Fonda Lee</p>
       </div>
       <button>Add Book</button>
     </form>
--- client/components/CurrentlyReading.js
+++ client/components/CurrentlyReading.js
@@ -4,7 +4,6 @@ export default function CurrentlyReading() {
   return (
     <>
       <h1>Currently Reading</h1>
-      <button>Add Book</button>
       <AddBookForm />
     </>
   );
