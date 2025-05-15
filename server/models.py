9a2230383bb8e55a5ad000104d4d40d16f513984
kale <18kmkf@queensu.ca>
2025-05-15 14:25:57 -0400

    Adding a shelf model

--- server/models.py
+++ server/models.py
@@ -27,12 +27,7 @@ class User(db.Model):
         default=db.func.now(), onupdate=db.func.now()
     )
     # Defining relationships
-    user_books: Mapped[list["UserBook"]] = relationship(
-        "UserBook", back_populates="user"
-    )
-    books: Mapped[list["Book"]] = relationship(
-        "Book", secondary="user_books", viewonly=True, back_populates="users"
-    )
+    shelves: Mapped[list["Shelf"]] = relationship("Shelf", back_populates="user")
 
     # Helper functions
     def set_password(self, password):
@@ -42,43 +37,52 @@ class User(db.Model):
         return check_password_hash(self.password, password)
 
 
+class Shelf(db.Model):
+    __tablename__ = "shelves"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    shelf_type: Mapped[str] = mapped_column(db.String(50), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    # Defining Relationships
+    user: Mapped["User"] = relationship("User", back_populates="shelves")
+    books: Mapped[list["Book"]] = relationship("Book", back_populates="shelf")
+
+
 class Book(db.Model):
     __tablename__ = "books"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(db.String(255), nullable=False)
     author: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    shelf_id: Mapped[int] = mapped_column(ForeignKey("shelves.id"), nullable=False)
 
     # Defining relationships
-    user_books: Mapped[list["UserBook"]] = relationship(
-        "UserBook", back_populates="book"
-    )
-    users: Mapped[list["User"]] = relationship(
-        "User", secondary="user_books", viewonly=True, back_populates="books"
-    )
+    shelf: Mapped["Shelf"] = relationship("Shelf", back_populates="books")
 
     def to_dict(self):
         return {"id": self.id, "title": self.title, "author": self.author}
 
 
-class UserBook(db.Model):
-    __tablename__ = "user_books"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    shelf_type: Mapped[str] = mapped_column(db.String(50), nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
-    started_at: Mapped[datetime.datetime] = mapped_column(db.DateTime)
-    finished_at: Mapped[datetime.datetime] = mapped_column(db.DateTime)
+# class UserBook(db.Model):
+#     __tablename__ = "user_books"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     shelf_type: Mapped[str] = mapped_column(db.String(50), nullable=False)
+#     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+#     book_id: Mapped[int] = mapped_column(ForeignKey("books.id"))
+#     started_at: Mapped[datetime.datetime] = mapped_column(db.DateTime)
+#     finished_at: Mapped[datetime.datetime] = mapped_column(db.DateTime)
 
-    # Defining Relationships
-    user: Mapped["User"] = relationship("User", back_populates="user_books")
-    book: Mapped["Book"] = relationship("Book", back_populates="user_books")
+#     # Defining Relationships
+#     user: Mapped["User"] = relationship("User", back_populates="user_books")
+#     book: Mapped["Book"] = relationship("Book", back_populates="user_books")
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "shelf_type": self.shelf_type,
-            "user_id": self.user_id,
-            "book_id": self.book_id,
-            "started_at": self.started_at,
-            "finished_at": self.finished_at,
-        }
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "shelf_type": self.shelf_type,
+#             "user_id": self.user_id,
+#             "book_id": self.book_id,
+#             "started_at": self.started_at,
+#             "finished_at": self.finished_at,
+#         }
