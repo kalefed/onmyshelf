60789ac5e9b00250fad1fc1167bc596f4d99f607
kale <18kmkf@queensu.ca>
2025-05-26 09:57:09 -0400

    add subgrid on dashboard

--- client/app/page.js
+++ client/app/page.js
@@ -8,25 +8,36 @@ export default async function Home({ searchParams }) {
   const { showModal } = await searchParams;
 
   return (
-    <main className="mt-20">
+    <main className="mt-10">
       <header>
         <h1 className="text-2xl text-center font-semibold">
           Welcome back, Kaleigh
         </h1>
       </header>
       <section className="grid grid-cols-4 gap-4 grid-rows-6 mt-10 mx-4">
-        <section className="bg-white rounded-xl row-span-3 row-start-1 col-start-1 p-4">
+        <section className="col-start-1 col-span-4 grid grid-cols-subgrid">
+          <div className="bg-white rounded-xl p-4">
+            <h3 className="text-lg font-semibold">Reading Challenge</h3>
+          </div>
+          <div className="bg-white rounded-xl col-span-2 p-4">
+            <h3 className="text-lg font-semibold">Reading Streak</h3>
+          </div>
+          <div className="bg-white rounded-xl p-4">
+            <h3 className="text-lg font-semibold">Stats</h3>
+          </div>
+        </section>
+        <section className="bg-white rounded-xl row-span-3 row-start-2 col-start-1 p-4">
           <h3 className="text-lg font-semibold">Currently Reading</h3>
           <CurrentlyReading />
         </section>
-        <section className="bg-white rounded-xl row-span-3 row-start-1 col-start-2 p-4">
+        <section className="bg-white rounded-xl row-span-3 row-start-2 col-start-2 p-4">
           <h3 className="text-lg font-semibold">Up Next</h3>
           <UpNext />
         </section>
-        <section className="bg-white rounded-xl col-span-2 row-span-6 row-start-1 col-start-3 p-4">
+        <section className="bg-white rounded-xl col-span-2 row-span-6 row-start-2 col-start-3 p-4">
           <h3 className="text-lg font-semibold">Reading Stats</h3>
         </section>
-        <section className="bg-white rounded-xl col-span-2 row-span-3 col-start-1 row-start-4 p-4">
+        <section className="bg-white rounded-xl col-span-2 row-span-3 col-start-1 row-start-5 p-4">
           <h3 className="text-lg font-semibold">Want to Read</h3>
           <WantToRead />
         </section>
