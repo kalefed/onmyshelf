21e67868ab9a1e34394dcbbdae0951788109bcb7
kale <18kmkf@queensu.ca>
2025-05-26 09:30:07 -0400

    style the log in form

--- client/app/users/login/loginForm.js
+++ client/app/users/login/loginForm.js
@@ -27,19 +27,44 @@ export default function LoginForm() {
   };
 
   return (
-    <div>
+    <section className="mt-10">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" name="username" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required />
-        </div>
-        <button type="submit">Log In</button>
+        <fieldset className="grid grid-cols-1 gap-y-8">
+          <legend className="sr-only">Login form</legend>
+          <div>
+            <label className="block text-sm/6 font-medium" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="mt-2 rounded-md outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-dark-orange px-3 py-1.5 w-full"
+              id="username"
+              type="text"
+              name="username"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm/6 font-medium" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="mt-2 rounded-md outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-dark-orange px-3 py-1.5 w-full"
+              id="password"
+              type="password"
+              name="password"
+              required
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="bg-dark-orange rounded-xl px-4 py-2 hover:bg-light-orange text-sm w-fit"
+              type="submit"
+            >
+              Log In
+            </button>
+          </div>
+        </fieldset>
       </form>
-      <Link href="/users/register">Register instead</Link>
-    </div>
+    </section>
   );
 }
--- client/app/users/login/page.js
+++ client/app/users/login/page.js
@@ -1,11 +1,19 @@
 import LoginForm from "./loginForm";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div>
-      <div>
+    <main className="flex justify-center mt-20">
+      <section className="w-full max-w-md px-4 bg-white rounded-xl py-8">
+        <h1 className="text-2xl font-semibold">Log in</h1>
+        <p className="flex gap-2 text-sm mt-2">
+          Don't have an account?
+          <Link className="underline text-dark-orange" href="/users/register">
+            Register instead
+          </Link>
+        </p>
         <LoginForm />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
