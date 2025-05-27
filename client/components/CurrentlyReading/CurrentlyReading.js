92cee9620b4251d12f0d17770346efa692165f2a
kale <18kmkf@queensu.ca>
2025-05-27 13:54:39 -0400

    Add wip of horizontal scroll

--- client/components/CurrentlyReading/CurrentlyReading.js
+++ client/components/CurrentlyReading/CurrentlyReading.js
@@ -4,7 +4,7 @@ import CurrentlyReadingBooks from "./Books";
 export default async function CurrentlyReading() {
   return (
     <div>
-      <div className="flex flex-row items-center gap-x-8">
+      <div className="flex flex-row items-center gap-x-8 p-1 overflow-x-scroll">
         <CurrentlyReadingBooks />
         <button className="bg-dark-green hover:bg-light-green rounded-full p-2 h-fit">
           <Link href="/?showModal=true">add</Link>
